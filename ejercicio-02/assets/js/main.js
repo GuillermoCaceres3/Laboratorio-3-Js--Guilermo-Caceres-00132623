30061275df2654f703ced52f8a4e8ded59153129
GuillermoCaceres3 <00132623@uca.edu.sv>
2024-09-03 12:08:54 -0600

    Labo completo

--- ejercicio-02/assets/js/main.js
+++ ejercicio-02/assets/js/main.js
@@ -1,65 +1,76 @@
 
-const plus = () => {
-    let numeros = [requestNumber]
-    let resultado = parseInt(numeros[1]) + parseInt(numeros[2]);
-    return resultado;
-};
-const subtract = () =>{
-    let numeros = [requestNumber]
-    let resultado = parseInt(numeros[1]) - parseInt(numeros[2]);
-    return resultado;
-} ;
+    const plus = (numeros) => {
+        let resultado = parseInt(numeros[0]) + parseInt(numeros[1]);
+        return resultado;
+    };
 
-const multiply = () =>{
-    let numeros = [requestNumber]
-    let resultado = parseInt(numeros[1]) * parseInt(numeros[2]);
-    return resultado;
-} ;
-const split = () => {
-    let numeros = [requestNumber]
-    if(numeros[1]/numeros[2] === 0){
-        return "No se puede dividir por cero";
-    } else {
-        let resultado = parseInt(numeros[1]) / parseInt(numeros[2]);
+    const subtract = (numeros) =>{
+        let resultado = parseInt(numeros[0]) - parseInt(numeros[1]);
+        return resultado;
+    } ;
+
+    const multiply = (numeros) =>{
+        let resultado = parseInt(numeros[0]) * parseInt(numeros[1]);
+        return resultado;
+    } ;
+
+    const split = (numeros) => {
+        let resultado;
+        if(parseInt(numeros[0]/parseInt(numeros[1]) === 0 )){
+            resultado = NaN;
+        } else{
+            resultado = parseInt(numeros[0]) / parseInt(numeros[1]);
+        }
+
+        return resultado;
+    };
+
+    const requestNumber = () => {
+        let valor1 = prompt("Escribe el primer numero",1);
+        let valor2 = prompt("Escribe el segundo numero",1);
+        
+        return [parseInt(valor1),parseInt(valor2)];
+    };
+
+    const selectOperation = (numeros) => {
+        let resultado;
+        let seleccion = prompt("Escribe tu seleccion: " + " " +
+            "+. Suma" + " " +
+            "-. Resta" + " " +
+            "*. Multiplicación" + " " +
+            "/. División"
+        )
+        switch(seleccion){
+            case "+":
+                resultado = plus(numeros);
+            break;
+            case "-":
+                resultado = subtract(numeros);
+            break;
+            case "*":
+                resultado = multiply(numeros);
+            break;
+            case "/": 
+                resultado = split(numeros);
+            break;
+            default:
+                resultado = "p";
+                alert("La opcion no es valida")
+            break;
+        }
+        return resultado;
+    };
+
+    const main = () => {
+        let numeros = requestNumber();  
+        let resultado = selectOperation(numeros);
+        if(resultado === "p"){
+            return;
+        }else if(isNaN(resultado)){
+            alert("No se puede dividir entre 0")
+        } else {
+            alert("El resultado es: " + `${resultado}`)
+        }
     }
-    return resultado;
-};
-const requestNumber = () => {
-    let valor1 = prompt("Escribe el primer numero",1);
-    let valor2 = prompt("Escribe el segundo numero",1);
-    
-    return [parseInt(valor1),parseInt(valor2)];
-};
 
-const selectOperation = () => {
-    let resultado;
-    let seleccion = prompt("Escribe tu seleccion: " + " " +
-        "+. Suma" + " " +
-        "-. Resta" + " " +
-        "*. Multiplicación" + " " +
-        "/. División"
-    )
-    switch(seleccion){
-        case "+":
-            resultado = plus();
-        break;
-        case "-":
-            resultado = subtract();
-        break;
-        case "*":
-            resultado = multiply();
-        break;
-        case "/": 
-            resultado = split();
-        break;
-    }
-    return resultado;
-};
-
-const main = () => {
-    let numbers = [requestNumber()]  
-    let resultado = selectOperation();
-    alert("El resultado es: " + `${resultado}`)
-}
-
-main();
+    main();
--- ejercicio-03/assets/js/main.js
+++ ejercicio-03/assets/js/main.js
@@ -1,13 +1,24 @@
-const calculateFactorial = (/*recibe*/) => {
-    //Code
+const calculateFactorial = (numero) => {
+    result = 1;
+    for(let i = numero; i > 1; i--){
+
+        result *= i;
+    }
+    return result;
 }
 
 const requestNumber = () => {
-    //Code
+    let valor = prompt("Ingresa un numero no negativo para calcular su factorial:")
+    if(parseInt(valor) < 0 ){
+        alert("El numero debe ser positivo")
+    } 
+    return parseInt(valor);
 }
 
 const main = () => {
-    //Code
+    let numero = requestNumber();
+    let resultado = calculateFactorial(numero);
+    alert("El factorial del numero es " + resultado )
 }
 
 main();
